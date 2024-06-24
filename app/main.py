f78e864991f4899d1a2ebde4c4c76c85a04ffcc4
Oscar Rojas <ozkrrr9@gmail.com>
2024-06-23 21:50:04 -0400

    pass stage

--- app/main.py
+++ app/main.py
@@ -2,17 +2,29 @@
 import socket
 
 
-def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
+def msg_estrucutura(data):
+    data_linea = data.splitlines()
+    data_path = data_linea[0].split()
 
-    # Uncomment this to pass the first stage
-    #
-    with socket.create_server(("localhost", 4221), reuse_port=True) as server_socket:
-        conexion, direccion = server_socket.accept()  # wait for client
-        print(f"accepted connection from {direccion}")
-        data = conexion.recv(1024)
+    return data_path[1]
+
+
+def manejo_respuesta(path, conexion):
+    if path == '/':
         conexion.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    else:
+        conexion.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
+
+
+def main():
+    # crea un servidor socket
+    with socket.create_server(("localhost", 4221)) as server_socket:
+        # acepta una conexion al socket y un ip
+        conexion, direccion = server_socket.accept()
+        data = conexion.recv(1024).decode()  # recopila la data de la peticion
+
+        path = msg_estrucutura(data)  # obtenet path
+        manejo_respuesta(path, conexion)  # manejar codigo html
 
 
 if __name__ == "__main__":
